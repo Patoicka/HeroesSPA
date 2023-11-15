74c68ccd6599eee8532aeaf21853d6c7556f0ba1
Isaac <espinosalejandro12003@gmail.com>
2023-11-14 23:51:35 -0600

    first commit

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,6 @@
+
+export const Hero = () => {
+  return (
+    <h1 className="color-primary">HeroPage</h1>
+  )
+}
---
+++ src/heroes/pages/SearchPage.jsx
@@ -0,0 +1,6 @@
+
+export const Search = () => {
+  return (
+    <h1 className="color-primary">SearchPage</h1>
+  )
+}
